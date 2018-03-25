760eb42956f29e4fead4aa50088c70aa24fa8434
Daniel Phillips <dphildev@gmail.com>
2018-03-25 20:19:53 +0100

    Forward pass of batch normalization. (Did not expect to work this out from the paper!)

--- cs231n/layers.py
+++ cs231n/layers.py
@@ -184,7 +184,12 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # variance, storing your result in the running_mean and running_var   #
         # variables.                                                          #
         #######################################################################
-        pass
+        sample_mean = np.mean(x, axis=0)
+        sample_var = np.mean((x - sample_mean) ** 2, axis=0)
+        running_mean = momentum * running_mean + (1 - momentum) * sample_mean
+        running_var = momentum * running_var + (1 - momentum) * sample_var
+        x_norm = (x - sample_mean) / np.sqrt(sample_var + eps)
+        out = gamma * x_norm + beta
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -195,7 +200,8 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # then scale and shift the normalized data using gamma and beta.      #
         # Store the result in the out variable.                               #
         #######################################################################
-        pass
+        x_norm = (x - running_mean) / np.sqrt(running_var + eps)
+        out = gamma * x_norm + beta
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
